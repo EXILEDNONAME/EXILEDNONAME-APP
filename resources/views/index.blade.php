e82a31d10bc6a98f37778f7c90e18fc70781ef3e
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2022-07-14 07:37:32 +0700

    Update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -105,75 +105,12 @@
           <div class="toolbar" id="kt_toolbar">
             <div id="kt_toolbar_container" class="container-fluid d-flex flex-stack">
               @include('pages.backend.includes.breadcrumb')
-
-              <div class="d-flex align-items-center gap-2 gap-lg-3">
-                <div class="m-0">
-                  <a href="#" class="btn btn-sm btn-flex btn-light btn-active-primary fw-bolder" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
-                    <span class="svg-icon svg-icon-5 svg-icon-gray-500 me-1">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                        <path d="M19.0759 3H4.72777C3.95892 3 3.47768 3.83148 3.86067 4.49814L8.56967 12.6949C9.17923 13.7559 9.5 14.9582 9.5 16.1819V19.5072C9.5 20.2189 10.2223 20.7028 10.8805 20.432L13.8805 19.1977C14.2553 19.0435 14.5 18.6783 14.5 18.273V13.8372C14.5 12.8089 14.8171 11.8056 15.408 10.964L19.8943 4.57465C20.3596 3.912 19.8856 3 19.0759 3Z" fill="currentColor" />
-                      </svg>
-                    </span>
-                    Filter</a>
-                    <div class="menu menu-sub menu-sub-dropdown w-250px w-md-300px" data-kt-menu="true" id="kt_menu_624445ab67cc1">
-                      <!--begin::Header-->
-                      <div class="px-7 py-5">
-                        <div class="fs-5 text-dark fw-bolder">Filter Options</div>
-                      </div>
-                      <div class="separator border-gray-200"></div>
-                      <div class="px-7 py-5">
-                        <div class="mb-10">
-                          <label class="form-label fw-bold">Status:</label>
-                          <div>
-                            <select class="form-select form-select-solid" data-kt-select2="true" data-placeholder="Select option" data-dropdown-parent="#kt_menu_624445ab67cc1" data-allow-clear="true">
-                              <option></option>
-                              <option value="1">Approved</option>
-                              <option value="2">Pending</option>
-                              <option value="2">In Process</option>
-                              <option value="2">Rejected</option>
-                            </select>
-                          </div>
-                        </div>
-                        <div class="mb-10">
-                          <label class="form-label fw-bold">Member Type:</label>
-                          <div class="d-flex">
-                            <label class="form-check form-check-sm form-check-custom form-check-solid me-5">
-                              <input class="form-check-input" type="checkbox" value="1" />
-                              <span class="form-check-label">Author</span>
-                            </label>
-                            <label class="form-check form-check-sm form-check-custom form-check-solid">
-                              <input class="form-check-input" type="checkbox" value="2" checked="checked" />
-                              <span class="form-check-label">Customer</span>
-                            </label>
-                          </div>
-                        </div>
-                        <div class="mb-10">
-                          <label class="form-label fw-bold">Notifications:</label>
-                          <div class="form-check form-switch form-switch-sm form-check-custom form-check-solid">
-                            <input class="form-check-input" type="checkbox" value="" name="notifications" checked="checked" />
-                            <label class="form-check-label">Enabled</label>
-                          </div>
-                        </div>
-                        <div class="d-flex justify-content-end">
-                          <button type="reset" class="btn btn-sm btn-light btn-active-light-primary me-2" data-kt-menu-dismiss="true">Reset</button>
-                          <button type="submit" class="btn btn-sm btn-primary" data-kt-menu-dismiss="true">Apply</button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <!--end::Actions-->
-              </div>
-              <!--end::Container-->
             </div>
-            <!--end::Toolbar-->
-            <!--begin::Post-->
-            <div class="post d-flex flex-column-fluid" id="kt_post">
-              <!--begin::Container-->
-              <div id="kt_content_container" class="container-xxl">
-                <!--begin::Card-->
-                <div class="card">
-
+          </div>
+          <div class="post d-flex flex-column-fluid" id="kt_post">
+            <div id="kt_content_container" class="container-xxl">
+              <div class="card">
+                <div class="card-body">
                   <table id="example" class="table table-striped" style="width:100%">
                     <thead>
                       <tr>
@@ -246,155 +183,31 @@
                       </tr>
                     </tfoot>
                   </table>
-
-                  <div class="card-header border-0 pt-6">
-                    <div class="card-title">
-                      <div class="d-flex align-items-center position-relative my-1">
-                        <span class="svg-icon svg-icon-1 position-absolute ms-6">
-                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                            <rect opacity="0.5" x="17.0365" y="15.1223" width="8.15546" height="2" rx="1" transform="rotate(45 17.0365 15.1223)" fill="currentColor" />
-                            <path d="M11 19C6.55556 19 3 15.4444 3 11C3 6.55556 6.55556 3 11 3C15.4444 3 19 6.55556 19 11C19 15.4444 15.4444 19 11 19ZM11 5C7.53333 5 5 7.53333 5 11C5 14.4667 7.53333 17 11 17C14.4667 17 17 14.4667 17 11C17 7.53333 14.4667 5 11 5Z" fill="currentColor" />
-                          </svg>
-                        </span>
-                        <input type="text" data-kt-user-table-filter="search" class="form-control form-control-solid w-250px ps-14" placeholder="Search user" />
-                      </div>
-                    </div>
-                    <div class="card-toolbar">
-                      <div class="d-flex justify-content-end" data-kt-user-table-toolbar="base">
-                        <button type="button" class="btn btn-light-primary me-3" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
-                          <span class="svg-icon svg-icon-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                              <path d="M19.0759 3H4.72777C3.95892 3 3.47768 3.83148 3.86067 4.49814L8.56967 12.6949C9.17923 13.7559 9.5 14.9582 9.5 16.1819V19.5072C9.5 20.2189 10.2223 20.7028 10.8805 20.432L13.8805 19.1977C14.2553 19.0435 14.5 18.6783 14.5 18.273V13.8372C14.5 12.8089 14.8171 11.8056 15.408 10.964L19.8943 4.57465C20.3596 3.912 19.8856 3 19.0759 3Z" fill="currentColor" />
-                            </svg>
-                          </span>
-                          Filter</button>
-                          <div class="menu menu-sub menu-sub-dropdown w-300px w-md-325px" data-kt-menu="true">
-                            <div class="px-7 py-5">
-                              <div class="fs-5 text-dark fw-bolder">Filter Options</div>
-                            </div>
-                            <div class="separator border-gray-200"></div>
-                            <div class="px-7 py-5" data-kt-user-table-filter="form">
-                              <div class="mb-10">
-                                <label class="form-label fs-6 fw-bold">Role:</label>
-                                <select class="form-select form-select-solid fw-bolder" data-kt-select2="true" data-placeholder="Select option" data-allow-clear="true" data-kt-user-table-filter="role" data-hide-search="true">
-                                  <option></option>
-                                  <option value="Administrator">Administrator</option>
-                                  <option value="Analyst">Analyst</option>
-                                  <option value="Developer">Developer</option>
-                                  <option value="Support">Support</option>
-                                  <option value="Trial">Trial</option>
-                                </select>
-                              </div>
-                              <div class="mb-10">
-                                <label class="form-label fs-6 fw-bold">Two Step Verification:</label>
-                                <select class="form-select form-select-solid fw-bolder" data-kt-select2="true" data-placeholder="Select option" data-allow-clear="true" data-kt-user-table-filter="two-step" data-hide-search="true">
-                                  <option></option>
-                                  <option value="Enabled">Enabled</option>
-                                </select>
-                              </div>
-                              <div class="d-flex justify-content-end">
-                                <button type="reset" class="btn btn-light btn-active-light-primary fw-bold me-2 px-6" data-kt-menu-dismiss="true" data-kt-user-table-filter="reset">Reset</button>
-                                <button type="submit" class="btn btn-primary fw-bold px-6" data-kt-menu-dismiss="true" data-kt-user-table-filter="filter">Apply</button>
-                              </div>
-                            </div>
-                          </div>
-
-                        </div>
-                        <div class="d-flex justify-content-end align-items-center d-none" data-kt-user-table-toolbar="selected">
-                          <div class="fw-bolder me-5">
-                            <span class="me-2" data-kt-user-table-select="selected_count"></span>Selected</div>
-                            <button type="button" class="btn btn-danger" data-kt-user-table-select="delete_selected">Delete Selected</button>
-                          </div>
-                        </div>
-                      </div>
-
-                      <div class="card-body py-4">
-                        <table class="table align-middle table-row-dashed fs-6 gy-5" id="kt_table_users">
-                          <thead>
-                            <tr class="text-start text-muted fw-bolder fs-7 text-uppercase gs-0">
-                              <th class="w-10px pe-2">
-                                <div class="form-check form-check-sm form-check-custom form-check-solid me-3">
-                                  <input class="form-check-input" type="checkbox" data-kt-check="true" data-kt-check-target="#kt_table_users .form-check-input" value="1" />
-                                </div>
-                              </th>
-                              <th class="min-w-125px">User</th>
-                              <th class="min-w-125px">Role</th>
-                              <th class="min-w-125px">Last login</th>
-                              <th class="min-w-125px">Two-step</th>
-                              <th class="min-w-125px">Joined Date</th>
-                              <th class="text-end min-w-100px">Actions</th>
-                            </tr>
-                          </thead>
-                          <tbody class="text-gray-600 fw-bold">
-                            <tr>
-                              <td>
-                                <div class="form-check form-check-sm form-check-custom form-check-solid">
-                                  <input class="form-check-input" type="checkbox" value="1" />
-                                </div>
-                              </td>
-                              <td class="d-flex align-items-center">
-                                <div class="symbol symbol-circle symbol-50px overflow-hidden me-3">
-                                  <a href="../../demo1/dist/apps/user-management/users/view.html">
-                                    <div class="symbol-label">
-                                      <img src="/assets/backend/media/avatars/300-6.jpg" alt="Emma Smith" class="w-100" />
-                                    </div>
-                                  </a>
-                                </div>
-                                <div class="d-flex flex-column">
-                                  <a href="../../demo1/dist/apps/user-management/users/view.html" class="text-gray-800 text-hover-primary mb-1">Emma Smith</a>
-                                  <span>smith@kpmg.com</span>
-                                </div>
-                              </td>
-                              <td>Administrator</td>
-                              <td><div class="badge badge-light fw-bolder">Yesterday</div></td>
-                              <td></td>
-                              <td>05 May 2022, 2:40 pm</td>
-                              <td class="text-end">
-                                <a href="#" class="btn btn-light btn-active-light-primary btn-sm" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">Actions
-                                  <span class="svg-icon svg-icon-5 m-0">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                      <path d="M11.4343 12.7344L7.25 8.55005C6.83579 8.13583 6.16421 8.13584 5.75 8.55005C5.33579 8.96426 5.33579 9.63583 5.75 10.05L11.2929 15.5929C11.6834 15.9835 12.3166 15.9835 12.7071 15.5929L18.25 10.05C18.6642 9.63584 18.6642 8.96426 18.25 8.55005C17.8358 8.13584 17.1642 8.13584 16.75 8.55005L12.5657 12.7344C12.2533 13.0468 11.7467 13.0468 11.4343 12.7344Z" fill="currentColor" />
-                                    </svg>
-                                  </span>
-                                </a>
-                                <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-bold fs-7 w-125px py-4" data-kt-menu="true">
-                                  <div class="menu-item px-3">
-                                    <a href="../../demo1/dist/apps/user-management/users/view.html" class="menu-link px-3">Edit</a>
-                                  </div>
-                                  <div class="menu-item px-3">
-                                    <a href="#" class="menu-link px-3" data-kt-users-table-filter="delete_row">Delete</a>
-                                  </div>
-                                </div>
-                              </td>
-                            </tr>
-                          </tbody>
-                        </table>
-                      </div>
-                    </div>
-                  </div>
                 </div>
+
               </div>
-              @include('pages.backend.includes.footer')
             </div>
           </div>
         </div>
+        @include('pages.backend.includes.footer')
+      </div>
+    </div>
+  </div>
 
-        @include('pages.backend.includes.drawer-activity')
-        @include('pages.backend.includes.drawer-chat')
-        @include('pages.backend.includes.drawer-demo')
-        @include('pages.backend.includes.drawer-help')
+  @include('pages.backend.includes.drawer-activity')
+  @include('pages.backend.includes.drawer-chat')
+  @include('pages.backend.includes.drawer-demo')
 
-        <div class="engage-toolbar d-flex position-fixed px-5 fw-bolder zindex-2 top-50 end-0 transform-90 mt-20 gap-2">
-          <button id="kt_engage_demos_toggle" class="engage-demos-toggle btn btn-flex h-35px bg-body btn-color-gray-700 btn-active-color-gray-900 shadow-sm fs-6 px-4 rounded-top-0" title="Check out 22 more demos" data-bs-toggle="tooltip" data-bs-placement="left" data-bs-dismiss="click" data-bs-trigger="hover">
-            <span id="kt_engage_demos_label">Demos</span>
-          </button>
-          <button id="kt_help_toggle" class="engage-help-toggle btn btn-flex h-35px bg-body btn-color-gray-700 btn-active-color-gray-900 shadow-sm px-5 rounded-top-0" title="Learn &amp; Get Inspired" data-bs-toggle="tooltip" data-bs-placement="left" data-bs-dismiss="click" data-bs-trigger="hover">Help</button>
-          <a href="https://1.envato.market/EA4JP" target="_blank" class="engage-purchase-link btn btn-color-gray-700 bg-body btn-active-color-gray-900' btn-flex h-35px px-5 shadow-sm rounded-top-0">Buy now</a>
-        </div>
+  <div class="engage-toolbar d-flex position-fixed px-5 fw-bolder zindex-2 top-50 end-0 transform-90 mt-20 gap-2">
+    <button id="kt_engage_demos_toggle" class="engage-demos-toggle btn btn-flex h-35px bg-body btn-color-gray-700 btn-active-color-gray-900 shadow-sm fs-6 px-4 rounded-top-0" title="Check out 22 more demos" data-bs-toggle="tooltip" data-bs-placement="left" data-bs-dismiss="click" data-bs-trigger="hover">
+      <span id="kt_engage_demos_label">Demos</span>
+    </button>
+    <a href="https://1.envato.market/EA4JP" target="_blank" class="engage-purchase-link btn btn-color-gray-700 bg-body btn-active-color-gray-900' btn-flex h-35px px-5 shadow-sm rounded-top-0">Buy now</a>
+  </div>
 
 
-        @include('pages.backend.includes.scroll-top')
-        @include('pages.backend.includes.js')
+  @include('pages.backend.includes.scroll-top')
+  @include('pages.backend.includes.js')
 
-      </body>
-      </html>
+</body>
+</html>
