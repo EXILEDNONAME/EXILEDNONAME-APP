c53bf815b039b84b3175658be1b3d564942d5cb0
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2022-07-18 11:30:55 +0700

    Update

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-- File Manager
-- Localization Language
+- Edit Profile
 - Datatable
 
 Success
 - Authentication Login ID, Phone & Email
+- File Manager
+- Localization Language
--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/dashboard', 'Backend\DashboardController@index')->name('index');
 Route::get('/dashboard/file-manager', 'Backend\DashboardController@file_manager')->name('file-manager');
+Route::get('/dashboard/language/{language}', 'Backend\DashboardController@language')->name('dashboard.language');
 Route::get('/dashboard/logout', 'Backend\DashboardController@logout')->name('logout');
